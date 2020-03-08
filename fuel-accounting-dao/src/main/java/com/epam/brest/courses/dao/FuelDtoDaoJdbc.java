6bbb66ad81b4d3da78bf5eb27768bbfb8b1d1ab8
Alexander Ignathick <alexanderignathick@gmail.com>
2020-03-08 15:35:58 +0300

    add FuelDtoDaoJdbc for getting summary fuel by transport fuel type

---
+++ fuel-accounting-dao/src/main/java/com/epam/brest/courses/dao/FuelDtoDaoJdbc.java
@@ -0,0 +1,18 @@
+package com.epam.brest.courses.dao;
+
+import com.epam.brest.courses.model.Fuel;
+import com.epam.brest.courses.model.dto.FuelDto;
+
+import java.util.List;
+
+/**
+ * Fuel DTO Service Interface Implementation.
+ */
+public class FuelDtoDaoJdbc implements FuelDtoDao {
+
+
+    @Override
+    public List<FuelDto> findAllWithFuelSum() {
+        return null;
+    }
+}
