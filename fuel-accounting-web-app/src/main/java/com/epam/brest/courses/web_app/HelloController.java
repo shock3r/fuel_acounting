38142f6139d08e9ab1dd4905781c594e2cedf5b4
Alexander Ignathick <alexanderignathick@gmail.com>
2020-03-10 00:01:20 +0300

    add hello controller to web-app

---
+++ fuel-accounting-web-app/src/main/java/com/epam/brest/courses/web_app/HelloController.java
@@ -0,0 +1,25 @@
+package com.epam.brest.courses.web_app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Hello MVC controller.
+ */
+@Controller
+public class HelloController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping(value = "/Hello")
+    public String hello2(@RequestParam(value = "name",required = false, defaultValue = "world") String name,
+                        Model model) {
+        LOGGER.debug("hello(name:{})", name);
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+}
---
+++ fuel-accounting-web-app/src/test/java/HelloControllerTest.java
@@ -0,0 +1,2 @@
+public class HelloControllerTest {
+}
