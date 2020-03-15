982a8d0dcf2265f9876557d8ec6c7886537674e4
Alexander Ignathick <alexanderignathick@gmail.com>
2020-03-16 00:50:12 +0300

    add fuelDtoService to FuelController, fix MockTest

--- fuel-accounting-web-app/src/main/java/com/epam/brest/courses/web_app/FuelController.java
+++ fuel-accounting-web-app/src/main/java/com/epam/brest/courses/web_app/FuelController.java
@@ -1,5 +1,6 @@
 package com.epam.brest.courses.web_app;
 
+import com.epam.brest.courses.service.FuelDtoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -13,6 +14,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class FuelController {
     private static final Logger LOGGER = LoggerFactory.getLogger(FuelController.class);
+    private final FuelDtoService fuelDtoService;
+
+    public FuelController(FuelDtoService fuelDtoService) {
+        this.fuelDtoService = fuelDtoService;
+    }
 
     /**
      *  Goto fuels list page.
@@ -22,6 +28,7 @@ public class FuelController {
     @GetMapping(value = "/fuels")
     public String fuels(Model model) {
         LOGGER.debug("fuels()");
+        model.addAttribute("fuels", fuelDtoService.findAllWithFuelSum());
         return "fuels";
     }
 
