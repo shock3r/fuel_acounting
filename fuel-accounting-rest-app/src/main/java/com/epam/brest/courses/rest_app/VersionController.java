475727c28cefa2a72017e3036bb4769684099a79
Alexander Ignathick <alexanderignathick@gmail.com>
2020-04-03 22:27:05 +0300

    remove swagger2

--- fuel-accounting-rest-app/src/main/java/com/epam/brest/courses/rest_app/VersionController.java
+++ fuel-accounting-rest-app/src/main/java/com/epam/brest/courses/rest_app/VersionController.java
@@ -2,13 +2,11 @@ package com.epam.brest.courses.rest_app;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Root controller.
  */
 @RestController
-@EnableSwagger2
 public class VersionController {
     private final static String VERSION = "0.0.1";
 
