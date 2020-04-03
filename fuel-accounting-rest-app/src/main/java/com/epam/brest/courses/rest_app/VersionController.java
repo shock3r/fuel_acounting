f53059a43135cc1e5fc09179dc28e35766dfb817
Alexander Ignathick <alexanderignathick@gmail.com>
2020-04-03 21:59:40 +0300

    make code review 2020-04-03

--- fuel-accounting-rest-app/src/main/java/com/epam/brest/courses/rest_app/VersionController.java
+++ fuel-accounting-rest-app/src/main/java/com/epam/brest/courses/rest_app/VersionController.java
@@ -4,11 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * Root controller.
+ */
 @RestController
 @EnableSwagger2
 public class VersionController {
     private final static String VERSION = "0.0.1";
 
+    /**
+     * Get current version.
+     *
+     * @return version
+     */
     @GetMapping(value = "/version")
     public String version() {
         return VERSION;
