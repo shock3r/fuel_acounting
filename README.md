1b22a27dcd89c39586b80003e1849b66b5fa7257
Alexander Ignathick <alexanderignathick@gmail.com>
2020-04-06 19:46:21 +0300

    find all fuels REST

--- README.md
+++ README.md
@@ -10,21 +10,48 @@ Setup java 11 and Maven, see [enviroment_setup.md](enviroment_setup.md)
 Goto Project folder and execute  
     
     mvn clean install
+
+## Rest Server
+### Start Rest using Maven Jetty plugin
+```
+mvn jetty:run -pl fuel-accounting-rest-app
+```    
+or
+ ``` 
+cd fuel-accounting-rest-app
+mvn jetty:run
+```
+
+## Available Rest endpoints
+### version
+    curl --location --request GET 'localhost:8088/version'
+### fuel dtos
+```
+curl --location --request GET 'localhost:8088/fuel_dtos'
+```
+Pretty print json:
+```
+curl --location --request GET 'localhost:8088/fuel_dtos' | json_pp       
+```
+### fuels
+#### findAll
+    curl --location --request GET 'localhost:8088/fuels' | json_pp
+#### findById(fuelId)
+    curl --location --request GET 'localhost:8088/fuel/1'| json_pp
     
-## Run jetty web server
-    mvn jetty:run -pl fuel-accounting-web-app
+## Start monolite web application using Maven Jetty plugin
+ ```
+ mvn jetty:run -pl fuel-accounting-web-app
+ ```    
+ or
+ ``` 
+ cd fuel-accounting-web-app
+ mvn jetty:run
+ ```
 
 ## Connect to web application using localhost or 127.0.0.1 adress and 8081 port
     Application was tested on browser google chrome 80.0.3987.132
     in path field use adress: "http://localhost:8081/".
     this adress will redirect you on fuels page: "http://localhost:8081/fuels"
     For date moment 2020_04_03 23:23 monolite web application is ready
-    
- ## Run jetty rest server
-     mvn jetty:run -pl fuel-accounting-rest-app 
-     for testing rest services
-     For date moment 2020_04_03 23:23 you can use:
-     get http:/localhost:8088/fuel_dtos 
-     get http:/localhost:8088/fuels
-     get http:/localhost:8088/fuel/{id}
-     post http:/localhost:8088/fuels 
+
---
+++ fuel-accounting-rest-app/src/test/java/com/epam/brest/courses/rest_app/FuelControllerIT.java
@@ -0,0 +1,167 @@
+package com.epam.brest.courses.rest_app;
+
+import com.epam.brest.courses.model.Fuel;
+import com.epam.brest.courses.rest_app.exception.CustomExceptionHandler;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.epam.brest.courses.constants.FuelConstants.FUEL_NAME_SIZE;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = {"classpath:app-context-test.xml"})
+public class FuelControllerIT {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(FuelControllerIT.class);
+
+    public static final String FUELS_ENDPOINT = "/fuels";
+
+    @Autowired
+    private FuelController fuelController;
+    @Autowired
+    private CustomExceptionHandler customExceptionHandler;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    private MockMvc mockMvc;
+
+    MockMvcFuelService fuelService = new MockMvcFuelService();
+
+    @BeforeEach
+    public void before() {
+        mockMvc = MockMvcBuilders.standaloneSetup(fuelController)
+                .setMessageConverters(new MappingJackson2HttpMessageConverter())
+                .setControllerAdvice(customExceptionHandler)
+                .alwaysDo(MockMvcResultHandlers.print())
+                .build();
+    }
+
+    @Test
+    public void shouldFindAllFuels() throws Exception {
+        List<Fuel> fuels = fuelService.findAll();
+        assertNotNull(fuels);
+        assertTrue(fuels.size() > 0);
+    }
+
+    @Test
+    public void shouldfindFuelById() {
+        // given
+        Fuel fuel = new Fuel()
+                .setFuelName(RandomStringUtils.randomAlphabetic(FUEL_NAME_SIZE));
+        Integer id = fuelService.create(fuel);
+
+        // when
+        Optional<Fuel> fuelOptional = fuelService.findById(id);
+
+        // then
+        assertTrue(fuelOptional.isPresent());
+        assertEquals(fuelOptional.get().getFuelId(), id);
+        assertEquals(fuelOptional.get().getFuelName(), fuel.getFuelName());
+    }
+
+    @Test
+    public void shouldCreateFuel() {
+        Fuel fuel = new Fuel()
+                .setFuelName(RandomStringUtils.randomAlphabetic(FUEL_NAME_SIZE));
+        Integer id = fuelService.create(fuel);
+        assertNotNull(id);
+    }
+
+    @Test
+    public void shouldUpdateFuel() {
+        // given
+        Fuel fuel = new Fuel()
+                .setFuelName(RandomStringUtils.randomAlphabetic(FUEL_NAME_SIZE));
+        Integer id = fuelService.create(fuel);
+        assertNotNull(id);
+
+        Optional<Fuel> fuelOptional = fuelService.findById(id);
+        assertTrue(fuelOptional.isPresent());
+
+        fuelOptional.get()
+                .setFuelName(RandomStringUtils.randomAlphabetic(FUEL_NAME_SIZE));
+
+        // when
+        int result = fuelService.update(fuelOptional.get());
+
+        // then
+        assertTrue(1 == result);
+        Optional<Fuel> updatedFuelOptional = fuelService.findById(id);
+        assertTrue(updatedFuelOptional.isPresent());
+        assertEquals(updatedFuelOptional.get().getFuelId(), id);
+        assertEquals(updatedFuelOptional.get().getFuelName(), fuelOptional.get().getFuelName());
+    }
+
+    @Test
+    public void shouldDeleteFuel() throws Exception {
+        //given
+        Fuel fuel = new Fuel()
+                .setFuelName(RandomStringUtils.randomAlphabetic(FUEL_NAME_SIZE));
+        Integer id = fuelService.create(fuel);
+        assertNotNull(id);
+
+        List<Fuel> fuels = fuelService.findAll();
+        assertNotNull(fuels);
+
+        // when
+        int result = fuelService.delete(id);
+
+        //then
+        assertTrue(1 == result);
+
+        List<Fuel> currentFuels = fuelService.findAll();
+        assertNotNull(currentFuels);
+
+        assertTrue(fuels.size() - 1 == currentFuels.size());
+    }
+
+    class MockMvcFuelService {
+
+        public List<Fuel> findAll() throws Exception {
+            LOGGER.debug("findAll()");
+            MockHttpServletResponse response = mockMvc.perform(get(FUELS_ENDPOINT)
+                    .accept(MediaType.APPLICATION_JSON)
+            ).andExpect(status().isOk())
+                    .andReturn().getResponse();
+            assertNotNull(response);
+            return objectMapper.readValue(response.getContentAsString(), new TypeReference<List<Fuel>>() {});
+        }
+
+        public Optional<Fuel> findById(Integer fuelId) {
+            return Optional.empty();
+        }
+
+        public Integer create(Fuel fuel) {
+            return null;
+        }
+
+        public int update(Fuel fuel) {
+            return 0;
+        }
+
+        public int delete(Integer fuelId) {
+            return 0;
+        }
+    }
+
+}
